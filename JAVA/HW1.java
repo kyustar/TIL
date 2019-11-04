0c29890ee84a9c9b01bb836068166842c07b838b
kyustar <jbk92812@gmail.com>
2019-11-05 02:51:22 +0900

    별찍기

---
+++ JAVA/HW1.java
@@ -0,0 +1,22 @@
+##1~10 범위의 난수를 받아 별찍기!
+
+import java.util.Random;
+
+
+public class HW1 {
+	public static void main(String[] args){
+	Random random = new Random();
+	
+	int num = random.nextInt(10)+1;
+	
+		for(int i=0;i<num;i++){
+			for(int j=0;j<=i;j++){
+				System.out.print("*");
+			}
+		System.out.println("");
+
+		}
+	}
+}
+
+
