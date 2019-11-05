1bf74961ff8c3a13c361cbceab22c1126b06129f
kyustar <jbk92812@gmail.com>
2019-11-05 12:55:07 +0900

    Update HW1.java

--- JAVA/HW1.java
+++ JAVA/HW1.java
@@ -1,20 +1,20 @@
-##1~10 범위의 난수를 받아 별찍기!
-
 import java.util.Random;
 
-
+/**
+ * 1~10 범위의 난수를 받아 별찍기!
+ */
 public class HW1 {
-	public static void main(String[] args){
-	Random random = new Random();
+	public static void main (String[] args) {
 	
-	int num = random.nextInt(10)+1;
+		Random random = new Random();
 	
-		for(int i=0;i<num;i++){
-			for(int j=0;j<=i;j++){
+		int num = random.nextInt(10) + 1;
+	
+		for (int i = 0; i < num; i++) {
+			for(int j = 0; j <= i; j++) {
 				System.out.print("*");
 			}
-		System.out.println("");
-
+			System.out.println("");
 		}
 	}
 }
